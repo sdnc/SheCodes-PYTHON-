354e824065bdc8dd00bdc5b8b4a8fd70a63a704c
sdnc <dza.sara@gmail.com>
2022-10-25 20:29:36 +1100

    Add loop exercise for week 1, day 1 PYTHON

---
+++ week1_tue/for_loops_playground.py
@@ -0,0 +1,30 @@
+print("----------------------------------------")
+
+for num in range(10):
+	print(num)
+
+print("----------------------------------------")
+
+for num in range(1, 11):
+        print(num)
+
+print("----------------------------------------")
+
+for num in range(0, 11, 2):
+        print(num)
+
+print("----------------------------------------")
+
+# index example of range(0, 1, 2)
+# if range has only 1 input, index 0 represent the int range will count up to (exclusive) starting with 0
+# if range has several inputs, index 0 represents where range will start to count from (inclusive)
+# the second input (index 1) represents where range will count up until (exclusive)
+# if there is a third input (index 2), it represents the steps range will count in (e.g. if third input = 2, it will count in steps of two)
+
+for num in range(0, 101):
+	print(num)
+
+print("----------------------------------------")
+
+for num in range(0, 101, 5):
+	print(num) 
